ebbd3dcdd9dcb788dbb6648a0b9de75d7112880e
Ravi Kant <purbeyravi123@gmail.com>
2026-01-22 15:29:15 +0000

    dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = 'rk@google.com'
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2 //not allowed
+
+console.log(accountId);
+accountEmail ="rav@gmail.com"
+accountCity = "Bengaluru"
+accountPassword="21212342"
+console.log(accountId)
+console.table([accountEmail,accountCity,accountPassword,accountId, accountState])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert("hello")  //we are using nodejs , not browser
+
+console.log("Ravi")
+console.log(3+3);
+
+let name = "Ravi"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// undefined =>
+    // symbol => unique
+
+// object
+
+console.log(typeof undefined);
