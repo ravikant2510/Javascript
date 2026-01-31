a791e411732e676dee3268e041af246037d3f4a7
Ravi Kant <purbeyravi123@gmail.com>
2026-01-31 04:56:54 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,12 +5,38 @@ if (true) {
    let a  = 10
    const b = 20
    var c = 30
-   console.log("Inner :",a);
+   // console.log("Inner :",a);
    
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one() {
+//    const username = "ravi"
+
+//    function two() {
+//       const website = "youtube"
+//       console.log(username);     
+//    }
+//    // console.log(website);
+//       two()
+   
+// }
+// one()
+
+if (true) {
+   const username = "Ravikant"
+   if (username == "Ravikant") {
+      const website = "youtube"
+      console.log(username+website);
+      
+   }
+  // console.log(website);
+   
+}
+// console.log(username);
+
 
 
