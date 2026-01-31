fbc9bf5f29d094038c033342ae74079501ef94c2
Ravi Kant <purbeyravi123@gmail.com>
2026-01-31 12:06:39 +0000

    iteration

---
+++ 05_iterations/three.js
@@ -0,0 +1,57 @@
+// for of
+
+// for...of (loop) is used for Works with:  Arrays  Strings  Maps  Sets  
+//(Anything iterable)
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+
+// for in
+// for...in Iterates over KEYS (indexes / property names) Works with: Objects Arrays
+
+
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
+
+
+
+
+
+
+
+
